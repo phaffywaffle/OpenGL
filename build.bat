4319d3422a99fdf65fd5faa0920c42882de83cc8
phaffywaffle <dallasmail1@gmail.com>
2016-09-09 14:01:30 -0700

    Just when you think you know it all. We've got a long way to go

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-c++ Main.cpp Model.cpp -o Main -I x:\flash\OpenGL\GLEW\include -I x:\flash\OpenGL\GLFW\include -I x:\flash\OpenGL\GLM -DGLEW_STATIC glew32s.lib -lglfw3 -lopengl32 -luser32 -lkernel32 -lgdi32
+c++ Main.cpp Model.cpp -o Main -I x:\flash\OpenGL\GLEW\include -I x:\flash\OpenGL\GLFW\include -I x:\flash\OpenGL\GLM -DGLEW_STATIC -lglew32 -lglfw3 -lopengl32 -luser32 -lkernel32 -lgdi32
