2137ca182f4e95964f6260b0447836df8ef1ae0d
phaffywaffle <dallasmail1@gmail.com>
2016-09-13 08:02:00 -0600

    Fixed the build file to suppress warnings and not link to pointless windows libraries

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-c++ Main.cpp Model.cpp -o Main -I x:\flash\OpenGL\GLEW\include -I x:\flash\OpenGL\GLFW\include -I x:\flash\OpenGL\GLM -DGLEW_STATIC -lglew32 -lglfw3 -lopengl32 -luser32 -lkernel32 -lgdi32
+c++ Main.cpp Model.cpp -w -o Main -I x:\flash\OpenGL\GLEW\include -I x:\flash\OpenGL\GLFW\include -I x:\flash\OpenGL\GLM  -lglew32 -lglfw3 -lopengl32 -lgdi32
