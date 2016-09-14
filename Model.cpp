0772d8866570c4ec47ae715e5767c9a08190e85f
phaffywaffle <dallasmail1@gmail.com>
2016-09-14 14:34:41 -0700

    More babysteps. Hopefully done with buffer size for good. Except The index buffer

--- Model.cpp
+++ Model.cpp
@@ -20,30 +20,13 @@ checkEqual(Vertex v1, Vertex v2)
 	return(v1.x == v2.x && v1.y == v2.y && v1.z == v2.z);
 }
 
-internal void
-processFace(std::string line)
-{
-	std::istringstream iss(line);
-
-	uint32 numbers[3 * 3];
-
-	iss >> garbagechar >> numbers[0] >> garbagechar >> numbers[1] >> garbagechar >> numbers[2] >> numbers[3] >> garbagechar >> numbers[4] >> garbagechar >> numbers[5] >> numbers[6] >> garbagechar >> numbers[7] >> garbagechar >> numbers[8];
-
-	Vertex v1 = {numbers[0], numbers[1], numbers[2]};
-	Vertex v2 = {numbers[3], numbers[4], numbers[5]};
-	Vertex v3 = {numbers[6], numbers[7], numbers[8]};
-
-	faces_vector.push_back(v1);
-	faces_vector.push_back(v2);
-	faces_vector.push_back(v3);
-}
-
 Model::Model()
 {
 	std::ifstream is("cube.obj");
 	if (is.bad()) { print("Error opening obj file"); pause(); exit(-1); }
 
 	num_verts = 0;
+	num_indices = 0;
 	std::string line;
 	while (std::getline(is, line))
 	{
@@ -53,7 +36,6 @@ Model::Model()
 			Vertex vertex;
 			iss >> garbagestring >> vertex.x >> vertex.y >> vertex.z;
 			vertices_vector.push_back(vertex);
-			num_verts++;
 		}
 		if (line[0] == 'v' && line[1] == 'n')
 		{
@@ -69,16 +51,37 @@ Model::Model()
 		}
 		if (line[0] == 'f')
 		{
-			processFace(line);
+			uint32 numbers[3*3];
+			iss >> garbagechar >> numbers[0] >> garbagechar >> numbers[1] >> garbagechar >> numbers[2] >> numbers[3] >> garbagechar >> numbers[4] >> garbagechar >> numbers[5] >> numbers[6] >> garbagechar >> numbers[7] >> garbagechar >> numbers[8];
+			// not dealing with ints properly
+			Vertex v1 = {numbers[0], numbers[1], numbers[2]};
+			Vertex v2 = {numbers[3], numbers[4], numbers[5]};
+			Vertex v3 = {numbers[6], numbers[7], numbers[8]};
+
+			faces_vector.push_back(v1);
+			faces_vector.push_back(v2);
+			faces_vector.push_back(v3);
 		}
 	}
 
 	for(int i = 0; i < faces_vector.size(); i++)
 	{
-		print("Face: " << faces_vector.at(i).x << "/" << faces_vector.at(i).y << "/" << faces_vector.at(i).z);	
+		bool isNew = true;
+		Vertex test = faces_vector.at(i);
+		for(int j = 0; j < i; j++)
+		{
+			Vertex v = faces_vector.at(j);
+			if(checkEqual(test, v))
+			{
+				isNew = false;
+			}
+		}
+		if(isNew) num_verts++;
 	}
 
-
+	verts = new real32[num_verts * FLOATS_PER_VERTEX];
+	uvs = new real32[num_verts * FLOATS_PER_UV];
+	normals = new real32[num_verts * FLOATS_PER_NORMAL];
 
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
